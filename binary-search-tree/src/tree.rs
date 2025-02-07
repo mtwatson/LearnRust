e24aea27a16bb13cf33d77fb897b5669ea8ca7e2
Mike <watsonmichael919@gmail.com>
2025-02-07 14:29:06 -0600

    incorporated Kevins code to bridge gap

--- binary-search-tree/src/tree.rs
+++ binary-search-tree/src/tree.rs
@@ -1,5 +1,6 @@
 use std::fmt::Debug;
 use std::ptr::NonNull;
+use either::Either;
 
 type UsizeLink = Option<NonNull<UsizeNode>>;
 
@@ -161,65 +162,148 @@ impl UsizeTree
 
         None // Node not found
     }
+    
+    fn find_link_mut(&mut self, elem: usize) -> &mut UsizeLink
+    {
+        unsafe {
+            let mut cur_node = &mut self.root;
+            while let Some(cur) = cur_node
+            {
+                let parent = &mut (*cur.as_ptr());
+                if elem == parent.elem
+                {
+                    return cur_node;
+                }
+
+                cur_node = if elem < parent.elem { &mut parent.left } else { &mut parent.right };
+            }
+            cur_node
+        }
+    }
+
+    fn find_link(&self, elem: usize) -> &UsizeLink
+    {
+        unsafe {
+            let mut cur_node = &self.root;
+            while let Some(cur) = cur_node
+            {
+                let parent = &mut (*cur.as_ptr());
+                if elem == parent.elem
+                {
+                    return cur_node;
+                }
+
+                cur_node = if elem < parent.elem { &mut parent.left } else { &mut parent.right };
+            }
+            cur_node
+        }
+    }
 
     // remove an element from the tree
     // return if removal was successful
     // removal fails if the value isn't in the tree
     pub fn remove(&mut self, elem: usize) -> bool
     {
-        // if let Some(found_node) = Self::find_node(self.root, elem) {
-        //     unsafe {
-        //         Self::drop_node(found_node);
-        //         return true
-        //     }
-        // }
-        false
+        unsafe {
+            let remove_link = self.find_link_mut(elem);
+
+            if remove_link.is_some()
+            {
+                let remove_node = &mut (*remove_link.unwrap().as_ptr());
+
+                if remove_node.left.is_none()
+                {
+                    let old = Box::from_raw(remove_link.unwrap().as_ptr());
+                    *remove_link = remove_node.right.take();
+                    drop(old);
+                }
+                else if remove_node.right.is_none()
+                {
+                    let old = Box::from_raw(remove_link.unwrap().as_ptr());
+                    *remove_link = remove_node.left.take();
+                    drop(old);
+                }
+                else
+                {
+                    let mut successor_link = &mut remove_node.right;
+                    while (*successor_link.unwrap().as_ptr()).left.is_some()
+                    {
+                        successor_link = &mut (*successor_link.unwrap().as_ptr()).left;
+                    }
+
+                    remove_node.elem = (*successor_link.unwrap().as_ptr()).elem;
+
+                    let successor_node = &mut (*successor_link.unwrap().as_ptr());
+                    let old = Box::from_raw(successor_link.unwrap().as_ptr());
+                    *successor_link = successor_node.right.take();
+                    drop(old);
+                }
+                self.len -= 1;
+                return true;
+            }
+
+            false
+        }
     }
+
 
     // clear the tree
     // postcondition: tree is empty
-     pub fn clear(&mut self) { 
+    //  pub fn clear(&mut self) { 
+    //     unsafe {
+    //         Self::drop_node(self.root);
+    //     }
+    //     self.len = 0;
+    // }
+    // clear the tree
+    // postcondition: tree is empty
+    pub fn clear(&mut self)
+    {
         unsafe {
-            Self::drop_node(self.root);
+            while let Some(root_node) = self.root
+            {
+                let root_elem = (*root_node.as_ptr()).elem;
+                self.remove(root_elem);
+            }
         }
-        self.len = 0;
     }
 
     // Helper function to free nodes recursively
-    unsafe fn drop_node(node: UsizeLink) {
-        if let Some(non_null_node) = node {
-            let boxed_node = Box::from_raw(non_null_node.as_ptr());
+    // unsafe fn drop_node(node: UsizeLink) {
+    //     if let Some(non_null_node) = node {
+    //         let boxed_node = Box::from_raw(non_null_node.as_ptr());
 
-            //I am being optimistic here and assuming the tree isn't length gorillion
-            Self::drop_node(boxed_node.left);
-            Self::drop_node(boxed_node.right);
-        }
-    }
+    //         //I am being optimistic here and assuming the tree isn't length gorillion
+    //         Self::drop_node(boxed_node.left);
+    //         Self::drop_node(boxed_node.right);
+    //     }
+    // }
 }
 
 impl Drop for UsizeTree {
-    fn drop(&mut self) {
-        unsafe {
-            Self::drop_node(self.root);
-        }
-    }
+    // fn drop(&mut self) {
+    //     unsafe {
+    //         Self::drop_node(self.root);
+    //     }
+    // }
+        fn drop(&mut self) { self.clear(); }    
 }
 
-impl Clone for UsizeTree
-{
-    fn clone(&self) -> Self
-    {
-        todo!();
-    }
-}
+// impl Clone for UsizeTree
+// {
+//     fn clone(&self) -> Self
+//     {
+//         todo!();
+//     }
+// }
 
-impl Debug for UsizeTree
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
-    {
-        todo!();
-    }
-}
+// impl Debug for UsizeTree
+// {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+//     {
+//         todo!();
+//     }
+// }
 
 #[cfg(test)]
 mod tests
@@ -245,8 +329,7 @@ mod tests
     {
         let mut tree = UsizeTree::new();
         tree.insert(1);
-        // assert_eq!(tree.insert(4), true);
-        assert!(true)
+        assert_eq!(tree.insert(4), true);
     }
 
     #[test]
@@ -446,7 +529,7 @@ mod tests
         assert_eq!(tree.contains(5), true);
         assert_eq!(tree.contains(6), true);
         assert_eq!(tree.contains(7), true);
-        assert_eq!(tree.len(), 6);
+        assert_eq!(tree.len(), 7);
 
         assert_eq!(tree.remove(5), true);
         assert_eq!(tree.contains(5), false);
